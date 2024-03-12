2458d06f455efa5c03e9664f5923b104530bb25e
starwarsfan1233 <nelsonbooth123.nb@gmail.com>
2024-03-11 19:13:06 -0700

    may need to reconsider side navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './reactStyle.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const App = () => {
   // State for managing the nav menu's visibility on mobile
@@ -13,38 +15,50 @@ const App = () => {
       <nav>
       <a href="#home" onclick={toggleMenu}><img src={`${process.env.PUBLIC_URL}/Personal Logo.png`} alt="Logo" /></a>
         <div className={isMenuVisible ? "nav-links show" : "nav-links"} id="navLinks">
-            <i class="fa fa-times" onclick={toggleMenu}></i>
+          <FontAwesomeIcon icon={faBars} onClick={toggleMenu} />
             <ul>
                 <li><a href="#about" onclick={toggleMenu}>ABOUT</a></li>
-                <li><a href="#projects" onclick={toggleMenu}>PROJECTS</a></li>
                 <li><a href="#skills" onclick={toggleMenu}>SKILLS</a></li>
-                <li><a href="#contact" onclick={toggleMenu}>CONTACT</a></li>
+                <li><a href="#projects" onclick={toggleMenu}>PROJECTS</a></li>
                 <li><a href={`${process.env.PUBLIC_URL}/Nelson_Booth.pdf`} download="Nelson_Booth.pdf">RESUME</a></li>
             </ul>
         </div>
-        <i class="fa fa-bars" onclick={toggleMenu}></i>
+        <FontAwesomeIcon icon={faBars} onClick={toggleMenu} />
       </nav>
+      
       <section id="home" className="header">  
         <div className="text-box">
           <h1>Hello there! I'm Nelson!</h1>
           <p className="p1">An experienced software developer with the tools and know-how to have a track record of delivering impressive projects known for their usefulness and scalability!<br />
           </p>
           <p className="p2">"Let the future tell the truth, and evaluate each one according to his work and accomplishments. <br/>The present is theirs; the future, for which I have really worked, is mine." - Nikola Tesla</p>
-          <a href="#about" id="HeaderButton" className="header-button">Click Here to Learn More About Me!</a>
         </div>
       </section>
-      <section id="about">
-        {/* About section content */}
+
+      <section id="about" className="about-header">
+
+        <div class="about-box">
+            <h2>Who am I?</h2>
+        </div>
+
       </section>
-      <section id="projects">
-        {/* Projects section content */}
+
+      <section id="skills" className="skills-header">
+
+        <div class="skills-box">
+            <h4>My Skills</h4>
+        </div>
+
       </section>
-      <section id="skills">
-        {/* Skills section content */}
+
+      <section id="projects" className="projects-header">
+
+        <div class="projects-box">
+            <h3>Browse My Projects</h3>
+        </div>
+
       </section>
-      <section id="contact">
-        {/* Contact section content */}
-      </section>
+      
     </div>
   );
 };
