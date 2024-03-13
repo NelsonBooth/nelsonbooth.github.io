0204424c771f47b708ad2b14d2331972d1a22857
starwarsfan1233 <nelsonbooth123.nb@gmail.com>
2024-03-12 18:40:28 -0700

    functioning side navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './reactStyle.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const App = () => {
   // State for managing the nav menu's visibility on mobile
@@ -10,20 +11,33 @@ const App = () => {
     setIsMenuVisible(!isMenuVisible);
   };
 
+  // Listen for window resize events
+  useEffect(() => {
+    const handleResize = () => {
+      if(window.innerWidth > 867 && window.innerHeight > 867) {
+        setIsMenuVisible(false); // set side navbar state to false
+      }
+    };
+    window.addEventListener('resize', handleResize)
+
+    // Cleanup the event listener
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div>
       <nav>
-      <a href="#home" onclick={toggleMenu}><img src={`${process.env.PUBLIC_URL}/Personal Logo.png`} alt="Logo" /></a>
-        <div className={isMenuVisible ? "nav-links show" : "nav-links"} id="navLinks">
-          <FontAwesomeIcon icon={faBars} onClick={toggleMenu} />
+      <a href="#home"><img src={`${process.env.PUBLIC_URL}/Personal Logo.png`} alt="Logo" /></a>
+        <div className={isMenuVisible ? "nav-links show" : "nav-links"}>
+          <FontAwesomeIcon icon={faXmark} onClick={toggleMenu} className="menu-icon"/>
             <ul>
-                <li><a href="#about" onclick={toggleMenu}>ABOUT</a></li>
-                <li><a href="#skills" onclick={toggleMenu}>SKILLS</a></li>
-                <li><a href="#projects" onclick={toggleMenu}>PROJECTS</a></li>
+                <li><a href="#about" onClick={toggleMenu}>ABOUT</a></li>
+                <li><a href="#skills" onClick={toggleMenu}>SKILLS</a></li>
+                <li><a href="#projects" onClick={toggleMenu}>PROJECTS</a></li>
                 <li><a href={`${process.env.PUBLIC_URL}/Nelson_Booth.pdf`} download="Nelson_Booth.pdf">RESUME</a></li>
             </ul>
         </div>
-        <FontAwesomeIcon icon={faBars} onClick={toggleMenu} />
+        <FontAwesomeIcon icon={faBars} onClick={toggleMenu} className="menu-icon"/>
       </nav>
       
       <section id="home" className="header">  
